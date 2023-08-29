20d73a72fcd1298944cd32044aefe669d09342a4
Android_Lucifer <89004415+CyberGodKing@users.noreply.github.com>
2023-08-29 23:33:11 +0100

    Update settings.py

--- broker/settings.py
+++ broker/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'webbroker.apps.WebbrokerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
